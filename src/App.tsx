7e2061dd868c7199c38061af29a31ce23819ca28
Sohum Chandra Srivastava <sohum611@gmail.com>
2025-03-31 22:17:22 +0530

    Revert "Update App.tsx"
    This reverts commit 60e4c96221e39c74c3a6a9536a59ecebc787f887.

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import VideoHero from './components/VideoHero';
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+
 import { ThemeProvider } from "@/hooks/useTheme";
 import Index from "./pages/Index";
 import Programs from "./pages/Programs";

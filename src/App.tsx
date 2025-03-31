7cc3ff2948cc8e6c7febe7a86eae8e0e07c450a2
Sohum Chandra Srivastava <sohum611@gmail.com>
2025-03-31 21:46:43 +0530

    fix: bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/toaster";  
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import VideoHero from './components/VideoHero';
 import { ThemeProvider } from "@/hooks/useTheme";
 import Index from "./pages/Index";
 import Programs from "./pages/Programs";
